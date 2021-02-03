9cbc6a33877aac4a5e43e3efd216bcabb8902ba3
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-02-03 13:41:56 -0800

    comments made

---
+++ models/Pizza.js
@@ -0,0 +1,49 @@
+const { Mongoose } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
+
+const { Schema, model } = require('Mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    topping: [],
+
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+//get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+});
+
+// creae the pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+//export the Pizza model
+module.exports = Pizza;
